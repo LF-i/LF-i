287d733507c461b1676081a17ab6b05e83991155
Lorenzo Furlani <70235773+LF-i@users.noreply.github.com>
2022-06-07 00:59:36 +0200

    Add files via upload

--- FlashLoan.hs
+++ FlashLoan.hs
@@ -2,18 +2,18 @@ module FlashLoan where
 
 import Base
 
+data Market = Mkt1 | Mkt2 | Mkt3 deriving (Show, Eq, Read)
+
 flashloanIO :: Double -> Currency -> Something -> Market -> Market -> ContractIO
 flashloanIO x k1 k2 m1 m2 = do
     date <- date
     zcbIO date (return x) (Currency k1)
-                    `andIO`
-                    zcbIO date (return x * convertIO (Currency k1) / convertIO_Mkt k2 m1) k2
-                    `andIO`
-                    giveIO (zcbIO date (return x * convertIO (Currency k1) / convertIO_Mkt k2 m2) k2)
-                    `andIO`
-                    giveIO (zcbIO date (return x) (Currency k1))
-
-data Market = Mkt1 | Mkt2 | Mkt3 deriving (Show, Eq, Read)
+        `andIO`
+        zcbIO date (return x * convertIO (Currency k1) / convertIO_Mkt k2 m1) k2
+        `andIO`
+        giveIO (zcbIO date (return x * convertIO (Currency k1) / convertIO_Mkt k2 m2) k2)
+        `andIO`
+        giveIO (zcbIO date (return x) (Currency k1))
 
 convertIO_Mkt :: Something -> Market -> IO Double
 convertIO_Mkt Good m
